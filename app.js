883586118e5612e6bbaa4c5507e3af327e87b8ab
Lilii <lilii-ann.older@voco.ee>
2022-10-22 14:11:27 +0300

    commit

---
+++ app.js
@@ -0,0 +1,48 @@
+//let
+//let name = "Lilii-Ann Older"
+//name = "new name"
+//const
+const surname = "Older"
+//arrays
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers)
+numbers.push(6)
+console.log(numbers)
+
+//objects
+const person = {
+    firstname: "Lilii",
+    surname: "Older"
+}
+console.log(person)
+console.log(person.firstname)
+console.log(person.surname)
+person.department = "IKT"
+console.log(person)
+//data types
+//Strings
+let name = "Lilii-Ann Older"
+console.log(name)
+
+let persent = 50.2
+console.log(persent)
+console.log(typeof persent)
+
+let isHere = true
+console.log(isHere)
+console.log(typeof isHere)
+
+//null
+const airplane = null
+console.log(airplane)
+console.log(typeof airplane)
+
+//symbol
+const sym = Symbol()
+console.log(sym)
+console.log(typeof sym)
+
+//undefined
+let test
+console.log(test)
+console.log(typeof test)
