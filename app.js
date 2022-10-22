a5e477bc98604c162bebfc74994003c5cbe79085
Lilii <lilii-ann.older@voco.ee>
2022-10-22 14:35:55 +0300

    commit 12

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 let val;
 
-val = document
-val = document.all
-val = document.all[6]
-val = document.all.length
+val = document.getElementById('task')
 
-val = document.head
-val = document.body
-val = document.doctype
-val = document.URL
-val = document.characterSet
-val = document.contentType
+const taskTitle = document.getElementById('task-title')
 
-val = document.forms
-val = document.forms[0]
-val = document.forms[0].method
-val = document.forms[0].id
-val = document.forms[0].action
+taskTitle.style.background = '#000'
+taskTitle.style.color = '#fff'
+taskTitle.style.padding = '15px'
 
-val = document.links
-val = document.links[1].className
+taskTitle.textContent = 'Defined Tasks'
+taskTitle.innerText = 'My Tasks'
+taskTitle.innerHTML ='<b>My Tasks</b>'
+
+val = document.querySelector('#task-title')
+val = document.querySelector('ul')
+const task = document.querySelector('ul li')
+task.style.color = 'blue'
+
+val = document.querySelector('li:last-child')
+//val = document.querySelector('li:nth-child(odd)').style.background = '#ccc'
+//val = document.querySelector('li:nth-child(even)').style.background = '#f4f4f4'
+
+val = document.querySelectorAll('li')
+oddLi = document.querySelectorAll('li:nth-child(odd)')
+//val = document.querySelectorAll('li:nth-child(even)').style.background = '#f4f4f4'
+
+oddLi.forEach((li) => {
+    li.style.background = '#dddd'
+})
 
 console.log(val)
