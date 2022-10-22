696a62b9b925ce494f72f943f71726f1f961587a
Lilii <lilii-ann.older@voco.ee>
2022-10-22 14:25:53 +0300

    commit 1

--- app.js
+++ app.js
@@ -1,48 +1,42 @@
-//let
-//let name = "Lilii-Ann Older"
-//name = "new name"
-//const
-const surname = "Older"
-//arrays
-const numbers = [1, 2, 3, 4, 5]
-console.log(numbers)
-numbers.push(6)
-console.log(numbers)
+let val
 
-//objects
-const person = {
-    firstname: "Lilii",
-    surname: "Older"
-}
-console.log(person)
-console.log(person.firstname)
-console.log(person.surname)
-person.department = "IKT"
-console.log(person)
-//data types
-//Strings
-let name = "Lilii-Ann Older"
-console.log(name)
+//number -> String
+val = String(555)
+val = String(4+4)
+val = String(5.8)
 
-let persent = 50.2
-console.log(persent)
-console.log(typeof persent)
+// boolean -> String
+val = String(true)
+val = String(new Date())
 
-let isHere = true
-console.log(isHere)
-console.log(typeof isHere)
+//null -> String
+val = String(null)
 
-//null
-const airplane = null
-console.log(airplane)
-console.log(typeof airplane)
-
-//symbol
-const sym = Symbol()
-console.log(sym)
-console.log(typeof sym)
+//array -> String
+val = String([1, 2, 3, 4, 5])
 
 //undefined
-let test
-console.log(test)
-console.log(typeof test)
+val = String(undefined)
+
+// toString()
+val = (true).toString()
+//string -> number
+val = Number("5")
+//boolean -> number
+val = Number(true)
+val = Number(false)
+//null -> number
+val = Number(null)
+//string -> number
+val = Number("test")
+//array -> number
+val = Number([1, 2, 3, 4, 5])
+
+//toString()
+val = (true).toString()
+//str -> int
+val = parseInt("100.3")
+// str -> float
+val = parseInt("100.40")
+
+console.log(val)
