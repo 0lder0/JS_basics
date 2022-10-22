7121d5dc034971bdf2c6a758b80f0e2e74146d96
Lilii <lilii-ann.older@voco.ee>
2022-10-22 14:27:18 +0300

    commit 2

--- app.js
+++ app.js
@@ -1,42 +1,21 @@
-let val
+const num1 = 100
+const num2 = 50
+let result
 
-//number -> String
-val = String(555)
-val = String(4+4)
-val = String(5.8)
+result = num1 + num2
+result = num1 - num2
+result = num1 * num2
+result = num1 / num2
+result = num1 % num2
 
-// boolean -> String
-val = String(true)
-val = String(new Date())
+result = Math.PI
+result = Math.round(2.5)
+result = Math.ceil(2.3)
+result = Math.floor(2.8)
+result = Math.sqrt(64)
+result = Math.abs(-3)
+result = Math.pow(8, 2)
 
-//null -> String
-val = String(null)
+result = Math.round(Math.random() * 11)
 
-//array -> String
-val = String([1, 2, 3, 4, 5])
-
-//undefined
-val = String(undefined)
-
-// toString()
-val = (true).toString()
-//string -> number
-val = Number("5")
-//boolean -> number
-val = Number(true)
-val = Number(false)
-//null -> number
-val = Number(null)
-//string -> number
-val = Number("test")
-//array -> number
-val = Number([1, 2, 3, 4, 5])
-
-//toString()
-val = (true).toString()
-//str -> int
-val = parseInt("100.3")
-// str -> float
-val = parseInt("100.40")
-
-console.log(val)
+console.log(result)
