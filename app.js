045bc44573ba88b097a28f250c303f465d0f4cd4
Lilii <lilii-ann.older@voco.ee>
2022-10-22 14:32:40 +0300

    commit 5

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-const firstname = 'Lilii'
-const surname = 'Older'
-const email = 'lilii-ann.older@voco.ee'
-const dep = 'IKT'
+const numbers1 = [43,54,78,23,77,15,2]
+const numbers2 = new Array(22, 56, 87, 99)
 
-let result = "<ul>"+
-    "<li><b>Name:</b>" + firstname + "</li>"+
-    "<li><b>Surname:</b>" + surname + "</li>"+
-    "<li><b>Email:</b>" + email + "</li>"+
-    "<li><b>Department:</b>" + dep + "</li>"+
-    '</ul>'
+let val
+val = numbers1.length
+val = numbers2.length
 
-result = `
-    <ul>
-        <li><b>Name:</b> ${firstname}</li>
-        <li><b>Surname:</b> ${surname}</li>
-        <li><b>Email:</b> ${email}</li>
-        <li><b>Department:</b> ${dep}</li>
-    </ul>`
+val = numbers1[3]
+val = numbers1.indexOf(36)
 
+val = Array.isArray(val)
+numbers1.push(250)
+numbers1.unshift(120)
+numbers1.pop()
+numbers1.shift()
 
-console.log(result)
-document.body.innerHTML = result
+val = numbers1.concat(numbers2)
+
+const fruits = ['pineapple','banana','apple']
+val = fruits.sort()
+
+val = numbers1.sort(function (x, y) {
+    return x + y
+})
