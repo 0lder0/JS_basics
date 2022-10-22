442df5672e3574dbf2b29e8f7cb424f287151ae7
Lilii <lilii-ann.older@voco.ee>
2022-10-22 14:36:26 +0300

    commit 13

--- app.js
+++ app.js
@@ -1,32 +1,15 @@
-let val;
+const li = document.createElement('li')
+li.className = 'collection-item'
+const text = document.createTextNode('Study element creation')
+li.appendChild(text)
+const a = document.createElement('a')
+a.className = 'blue-text text-darken-2 secondary-content'
+text = document.createTextNode('X')
+a.appendChild(text)
+a.setAttribute('herf', '#')
 
-val = document.getElementById('task')
+li.appendChild(a)
 
-const taskTitle = document.getElementById('task-title')
-
-taskTitle.style.background = '#000'
-taskTitle.style.color = '#fff'
-taskTitle.style.padding = '15px'
-
-taskTitle.textContent = 'Defined Tasks'
-taskTitle.innerText = 'My Tasks'
-taskTitle.innerHTML ='<b>My Tasks</b>'
-
-val = document.querySelector('#task-title')
-val = document.querySelector('ul')
-const task = document.querySelector('ul li')
-task.style.color = 'blue'
-
-val = document.querySelector('li:last-child')
-//val = document.querySelector('li:nth-child(odd)').style.background = '#ccc'
-//val = document.querySelector('li:nth-child(even)').style.background = '#f4f4f4'
-
-val = document.querySelectorAll('li')
-oddLi = document.querySelectorAll('li:nth-child(odd)')
-//val = document.querySelectorAll('li:nth-child(even)').style.background = '#f4f4f4'
-
-oddLi.forEach((li) => {
-    li.style.background = '#dddd'
-})
-
-console.log(val)
+const list = document.querySelector('ul')
+list.appendChild(li)
+console.log(li)
